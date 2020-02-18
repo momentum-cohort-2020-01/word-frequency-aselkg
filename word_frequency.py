55c8fc1981c628981a6b9d919f31eb215ab9135f
aselkg <aselekkg@gmail.com>
2020-02-18 06:39:12 -0500

    first commit

--- word_frequency.py
+++ word_frequency.py
@@ -6,16 +6,48 @@ STOP_WORDS = [
 
 
 def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
+    """Read in `file` and print out the frequency of words in that file.""" 
+    # open file 
+    with open(file) as file:
+        open_file = file.read() 
+    
+    import string
+    # remove punctuation
+    tr = str.maketrans('', '', string.punctuation)
+    remove_punc = open_file.translate(tr)
+
+    # normalize all words to lowercase
+    lower = remove_punc.lower()
+    words = lower.split(' ')
+
+    # remove "stop words" -- words used so frequently they are ignored
+    new_words = [word for word in words if word not in STOP_WORDS]
+    
+    # go through the file word by word and keep a count of how often each word is used
+    d = {}
+    for word in new_words: 
+        # Check if the word is already in dictionary 
+        if word in d: 
+            # Increment count of word by 1 
+            d[word] = d[word] + 1
+        else: 
+            # Add the word to dictionary with count 1 
+            d[word] = 1
+  
+# Print the contents of dictionary 
+    for key in list(d.keys()): 
+        print(key, ":", d[key]) 
+
+    
+        
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": # this is for calling file name 
     import argparse
     from pathlib import Path
 
     parser = argparse.ArgumentParser(
-        description='Get the word frequency in a text file.')
+        description='Get the word frequency in a text file.') 
     parser.add_argument('file', help='file to read')
     args = parser.parse_args()
 
